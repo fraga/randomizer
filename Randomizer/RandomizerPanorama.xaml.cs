523007908cdd027843e35248280152310a8b6fd7
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-06-20 20:01:35 -0400

    migrating some of the code to panorama, text boxes in settings

--- Randomizer/RandomizerPanorama.xaml.cs
+++ Randomizer/RandomizerPanorama.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Randomizer.Utils;
 
 namespace Randomizer
 {
@@ -15,6 +16,34 @@ namespace Randomizer
         public RandomizerPanorama()
         {
             InitializeComponent();
+        }
+
+        private void TxtMin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtMin.Text = String.Empty;
+        }
+
+        private void TxtMax_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtMax.Text = String.Empty;
+        }
+
+        private async void appBarButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RandomOrgV1 randomOrg = new RandomOrgV1();
+
+                //txtResult.Text = await randomOrg.IntegerRequest(Int32.Parse(txtMin.Text), Int32.Parse(txtMax.Text)).Request();
+            }
+            catch (RandoOrgMinGreaterThanMaxException exception)
+            {
+                //txtResult.Text = exception.Message;
+            }
+            catch (Exception exception)
+            {
+                //txtResult.Text = exception.Message;
+            }
         }
     }
 }
