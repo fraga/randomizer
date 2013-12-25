2c0f9f4394aead9f0fe6b89ea6e4296a1170d075
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-12-25 16:39:25 -0500

    Adds assembly version

---
+++ Randomizer.Utils/AssemblyVersion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Randomizer.Utils
+{
+    public class AssemblyVersion
+    {
+        public static string GetCurrentVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
